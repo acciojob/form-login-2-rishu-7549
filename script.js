72b65989db490feab8aa54094dce77172b0e1d35
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-06 16:07:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+document.getElementById("submit").addEventListener('click', function(){
+let fname = document.getElementById("fname").value;
+let lname = document.getElementById("lname").value;
+let phone = document.getElementById("pnumber").value;
+let email = document.getElementById("email").value;
+	
+	alert(`First Name: ${fname} Last Name: ${lname} Phone Number: ${phone} Email ID: ${email}`);
+	
+});
+
