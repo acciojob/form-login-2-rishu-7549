eae7291f2bc167754495662ca5473ef803d3a08b
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-06 16:44:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-document.getElementById("submit").addEventListener('click', function(){
-let fname = document.getElementById("firstName").value;
-let lname = document.getElementById("lastName").value;
-let phone = document.getElementById("phone").value;
-let email = document.getElementById("email").value;
+document.getElementById("submit").addEventListener('click', function(event){
+	event.preventDefault(); 
+	let fname = document.getElementById("firstName").value;
+	let lname = document.getElementById("lastName").value;
+	let phone = document.getElementById("phone").value;
+	let email = document.getElementById("email").value;
 	
 	alert(`First Name: ${fname} Last Name: ${lname} Phone Number: ${phone} Email ID: ${email}`);
 	
